44b1e4dc4b225e36571818b7ae58687d010353f3
fabriceadou5@gmail.com <fabriceadou5@gmail.com>
2025-01-07 18:48:00 +0000

    Home site party

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Aos from 'aos'
+import "aos/dist/aos.css";
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 const App = () => {
+
+  useEffect(() => {
+    Aos.init(
+      {
+        offset: 100,
+        duration: 700,
+        easing: "ease-in",
+        delay: 100,
+      }
+  )
+  })
+
   return (
-    <div className='bg-red-400'>App</div>
+    <div className='overflow-x-hidden'>
+      <Navbar />
+      <Home />
+    </div>
   )
 }
 
